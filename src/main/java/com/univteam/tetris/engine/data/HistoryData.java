ff03f87e13e6c38792b3e9b1521b0b29e8a0029e
fanpan26 <fanpan26@126.com>
2018-03-08 18:20:36 +0800

    移除特殊方法，提取到DataUtil

--- src/main/java/com/univteam/tetris/engine/data/HistoryData.java
+++ src/main/java/com/univteam/tetris/engine/data/HistoryData.java
@@ -23,18 +23,8 @@ public class HistoryData {
         this.version+=1;
     }
 
-    private String getData(Point[] points) {
-        if (points == null||points.length == 0) {
-            return "";
-        }
-
-        StringBuilder str = new StringBuilder();
-        for (Point point : points) {
-            str.append(point);
-            str.append("|");
-        }
-        String s = str.toString();
-        return s.substring(0,s.length() - 1);
+    private  String getData(Point[] points) {
+       return DataUtil.transData(points);
     }
 
     public  void setCleared(Point[] points){
