8b3811cec4aa21fa98658eb63b20f06bf2e6f737
fanpan26 <fanpan26@126.com>
2018-03-08 18:24:09 +0800

    移除layui

--- target/classes/page/js/game.js
+++ target/classes/page/js/game.js
@@ -1,4 +1,5 @@
 $(function () {
+    var currentRoomId =1;
     var user = {
         uid: (function () {
             var u = localStorage.getItem('tetris_uid');
@@ -14,15 +15,59 @@ $(function () {
         38: 'UP',
         37: 'LEFT',
         39: 'RIGHT',
-        40: 'DOWN'
+        40: 'DOWN',
+        13:'CHAT'
     }
     //页面操作
     document.onkeydown = function (e) {
         var op = code[e.keyCode];
         if (op) {
-            ws.send('CMD:' + op + ':1:' + user.uid);//CMD:OPERATE::ROOMID:USERID
+            if (op == 'CHAT') {
+                chat.send();
+            } else {
+                ws.send('CMD:' + op + ':' + currentRoomId + ':' + user.uid);//CMD:OPERATE::ROOMID:USERID
+            }
         }
     }
+    $(document).on('click','#btn_send',function () {
+       chat.send();
+    });
+
+    var countDown = {
+        interval:null,
+        getNumber:function(i){
+            switch (i){
+                case 3:
+                    return ['3,5','4,5','5,5','5,6','5,7','5,8','3,9','4,9','5,9','5,10','5,11','5,12','3,13','4,13','5,13',];
+                case 2:
+                    return ['3,5','4,5','5,5','5,6','5,7','5,8','3,9','4,9','5,9','3,10','3,11','3,12','3,13','4,13','5,13',];
+                case 1:
+                    return ['3,8','4,7','4,8','5,5','5,6','5,7','5,8','5,9','5,10','5,11','5,12','3,13','4,13','5,13','6,13','7,13'];
+            }
+            return '';
+        },
+        print:function (points) {
+            //清空之前的
+            var ctx = tetris.getCtx(user.uid);
+            ctx.clearRect(0,0,200,400);
+            tetris.drawDetail(points.join('|'),'green',user.uid);
+        },
+        count:function () {
+            var i = 3;
+            this.interval = setInterval(function () {
+                if(i == 0) {
+                    chat.append('游戏开始...');
+                    clearInterval(countDown.interval);
+                    if(ws){
+                        ws.send('CMD:START:'+currentRoomId+':'+user.uid);
+                    }
+                }else{
+                    chat.append('玩家已经就绪,准备开始.'+i+'...');
+                }
+                i--;
+            }, 1000);
+        }
+    };
 
     var size = 20;
     var clearColor = '#242736';
@@ -31,6 +76,8 @@ $(function () {
     var tetris = {
         context1: null,
         context2: null,
+        context3:null,
+        context4:null,
         getCtx: function (id) {
             var self = id == user.uid;
             if (self) {
@@ -47,6 +94,33 @@ $(function () {
                 return this.context2;
             }
         },
+        getCtxSmall:function (id) {
+            var self = id == user.uid;
+            if (self) {
+                if (this.context3 == null) {
+                    var canvas = document.getElementById('tetris_u1_pre');
+                    this.context3 = canvas.getContext('2d');
+                }
+                return this.context3;
+            } else {
+                if (this.context4 == null) {
+                    var canvas = document.getElementById('tetris_u2_pre');
+                    this.context4 = canvas.getContext('2d');
+                }
+                return this.context4;
+            }
+        },
+        drawSmall:function (x,y,c,id) {
+            y = parseInt(y) + 4;//向下移动几个坐标，画到中间去
+            x = parseInt(x) - 1;
+            var ctx = this.getCtxSmall(id);
+            var sSize = 9;
+            ctx.fillStyle = c || 'green';
+            ctx.strokeStyle = clearColor;
+            ctx.lineWidth = 2;
+            ctx.fillRect(x * sSize, y * sSize, sSize, sSize);
+            ctx.strokeRect(x * sSize, y * sSize, sSize, sSize);
+        },
         draw: function (x, y, c, id) {
             var ctx = this.getCtx(id);
             if (c == clearColor) {
@@ -59,36 +133,54 @@ $(function () {
                 ctx.strokeRect(x * size, y * size, size, size);
             }
         },
+        drawDetail:function (str,color,uid,small) {
+            console.log("准备开始画")
+            var xy;
+            if (str) {
+                if(small){
+                    var ctx = this.getCtxSmall(uid);
+                    //先清除掉上次显示的内容
+                    ctx.clearRect(0,0,90,90);
+                }
+                var arr = str.split('|');
+                for (var i = 0; i < arr.length; i++) {
+                    xy = arr[i].split(',');
+                    if(small){
+                        tetris.drawSmall(xy[0], xy[1], color, uid);
+                    }else {
+                        tetris.draw(xy[0], xy[1], color, uid);
+                    }
+                }
+            }
+        },
         handle: function (msg) {
             var add = msg.his.added;
             var clear = msg.his.cleared;
             var stop = msg.his.stoped;
             var uid = msg.uid;
 
-            var drawDetail = function (str, color, uid) {
-                var xy;
-                if (str) {
-                    var arr = str.split('|');
-                    for (var i = 0; i < arr.length; i++) {
-                        xy = arr[i].split(',');
-                        tetris.draw(xy[0], xy[1], color, uid)
-                    }
-                }
-            }
             //先clear后add
-            drawDetail(clear, clearColor, uid);
-            drawDetail(add, addColor, uid);
-            drawDetail(stop, stopColor, uid);
+            this.drawDetail(clear, clearColor, uid);
+            this.drawDetail(add, addColor, uid);
+            this.drawDetail(stop, stopColor, uid);
         },
         join:function (d) {
            var players = d.users;
+           console.log('是否满员：'+d.full);
            $(players).each(function (i,item) {
               if (item.uid ==user.uid){
                   $('#user_photo_u1').attr('src',item.photo);
               }else{
                   $('#user_photo_u2').attr('src',item.photo);
               }
-           })
+           });
+           if (d.full){
+               countDown.count();
+           }else{
+               setTimeout(function () {
+                   chat.append("系统：等待其他玩家加入...");
+               },1000);
+           }
         },
         score:function (d) {
             if(d.uid == user.uid){
@@ -96,16 +188,31 @@ $(function () {
             }else{
                 $('#user_score_u2').html(d.score);
             }
+        },
+        next:function (d) {
+            var uid = d.uid;
+            var points = d.points;
+            this.drawDetail(points,addColor,uid,true);
         }
     };
     var chat = {
-        append:function (d) {
-            if(d.msg) {
-                $('.chat ul').append('<li>\n' +
-                    '<span>' + chat.getTime() + '</span>\n' +
-                    '<p> ' + d.msg + '</p>\n' +
-                    '</li>');
+        append:function (d,t) {
+            var getMsg = function (msg) {
+                var reTag = /<(?:.|\s)*?>/g;
+                return msg.replace(reTag,"");
+            };
+
+            $('.chat ul').append('<p>\n' +
+                (t? '<span>' + chat.getTime() + '</span>\n':'') +
+                '<p> ' + getMsg(d.msg || d) + '</p>\n' +
+                '</li>');
+        },
+        send:function () {
+            var txt = $('#text_chat').val();
+            if (txt && ws) {
+                ws.send('CMD:CHAT:' + currentRoomId + ':' + user.uid + ':' + encodeURIComponent(txt));
             }
+            $('#text_chat').val('');
         },
         getTime:function () {
             var date = new Date();
@@ -130,20 +237,22 @@ $(function () {
         join: 0,
         block: 1,
         score: 2,
-        over: 3
+        over: 3,
+        next:4
     };
 
+    chat.append('正在连接服务器...',false);
     var ws = new WebSocket('ws://127.0.0.1:8886');
     ws.onopen = function (event) {
-        console.log('server started');
-        ws.send('CMD:JOIN:1:'+user.uid);
+        chat.append('连接成功...',false);
+        ws.send('CMD:JOIN:' + currentRoomId + ':' + user.uid);
     }
     ws.onmessage = function (event) {
         try {
             var j = JSON.parse(event.data);
             switch (j.t) {
                 case msgType.sys:
-                    chat.append(j.d);
+                    chat.append(j.d,true);
                     break;
                 case msgType.join:
                     tetris.join(j.d);
@@ -157,9 +266,15 @@ $(function () {
                 case msgType.over:
                     console.log(j);
                     break;
+                case msgType.next:
+                    tetris.next(j.d);
+                    break;
             }
         } catch (e) {
             console.log(e.message);
         }
+    }
+    ws.onerror = function (event) {
+        chat.append('服务器出现异常，请刷新页面重试', false);
     }
 })
